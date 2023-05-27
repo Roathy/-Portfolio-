92c3aeccb32c41745a325f25da1438811cf27ce3
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-05-27 17:59:00 -0600

    Add card #5, info, css ^ image.

--- DATA/cards-data.js
+++ DATA/cards-data.js
@@ -51,6 +51,19 @@ const cardsData = [
       },
     ],
   },
+  {
+    title: 'Tonic',
+    topics: ['CANOPY', 'Back End Dev', '2015'],
+    text: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    buttons: [
+      {
+        text: 'See project',
+        icon: 'icon',
+        url: '#',
+      },
+    ],
+  },
 ];
 
 export default cardsData;
