7428ed5c9aeb160f957c57c490a703d67a79d3ec
Julio Eduardo Vergara Martinez <gogczoro@gmail.com>
2023-05-27 10:58:29 -0600

    Create DATA folder and cards-data for organization

---
+++ DATA/cards-data.js
@@ -0,0 +1,56 @@
+const cardsData = [
+  {
+    title: "Tonic",
+    topics: ["CANOPY", "Back End Dev", "2015"],
+    text: `A daily selection of privately personalized reads; no accounts or sign-ups required.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+    buttons: [
+      {
+        text: "See project",
+        icon: "icon",
+        url: "url",
+      },
+    ],
+  },
+  {
+    title: "Multi-Post Stories",
+    topics: ["CANOPY", "Back End Dev", "2015"],
+    text: `A daily selection of privately personalized reads; no accounts or sign-ups required.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+    buttons: [
+      {
+        text: "See project",
+        icon: "icon",
+        url: "url",
+      },
+    ],
+  },
+  {
+    title: "Tonic",
+    topics: ["CANOPY", "Back End Dev", "2015"],
+    text: `A daily selection of privately personalized reads; no accounts or sign-ups required.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+    buttons: [
+      {
+        text: "See project",
+        icon: "icon",
+        url: "url",
+      },
+    ],
+  },
+  {
+    title: "Multi-Post Stories",
+    topics: ["CANOPY", "Back End Dev", "2015"],
+    text: `A daily selection of privately personalized reads; no accounts or sign-ups required.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+    buttons: [
+      {
+        text: "See project",
+        icon: "icon",
+        url: "url",
+      },
+    ],
+  },
+];
+
+export default cardsData;
